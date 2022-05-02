596147870d619a80bb95cdabddf91649a0bd4a2d
LanteiRaph <santamulantei8@gmail.com>
2022-05-02 11:21:45 +0300

    Changes for deplyment

--- evuka/settings.py
+++ evuka/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&t9jb8qglz3@1d0ue9tbsgkcv9o_@gc9t6@4o+kg_^=n-(^ur-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
