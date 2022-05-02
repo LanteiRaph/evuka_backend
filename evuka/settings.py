21abd4c9ba995030e69cc18d8619b9be9e960b4f
LanteiRaph <santamulantei8@gmail.com>
2022-05-02 11:46:19 +0300

    Updated setting cors urls for frontend

--- evuka/settings.py
+++ evuka/settings.py
@@ -143,12 +143,14 @@ CORS_ALLOWED_ORIGINS = [
 
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://evuka.herokuapp.com',
 
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://evuka.herokuapp.com',
 ]
 
 # Internationalization
